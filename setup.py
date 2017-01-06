be142e849db78b91aac1e0dd60ff83838ca88ccf
René Calles <rcalles@ea.com>
2017-01-06 16:49:46 +0100

    working on tox config

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
+from setuptools.command.test import test as TestCommand
 
 # To use a consistent encoding
+import sys
 from codecs import open
 from os import path
 
@@ -10,6 +12,18 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 
 class CustomInstallCommand(install):
@@ -51,9 +65,10 @@ setup(
     keywords='omemo encryption messaging profanity xmpp jabber',
     packages=find_packages(exclude=['deploy', 'docs', 'tests']),
     data_files=[('profanity_omemo_plugin', ['deploy/prof_omemo_plugin.py'])],
-    # install_requires=['python-omemo'],
+    install_requires=['python-omemo'],
+    tests_require=['pytest'],
     dependency_links=['git+https://github.com/omemo/python-omemo.git@158b0a236d93b10d9c3b7ecea6c53254967f7b01#egg=python-omemo'],  # noqa
 
     # Extend the install command with a post_install command
-    cmdclass={'install': CustomInstallCommand},
+    cmdclass={'install': CustomInstallCommand, 'test': PyTest},
 )
