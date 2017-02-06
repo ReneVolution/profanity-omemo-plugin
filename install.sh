224069b6e8b3738ad74fc3ed3f08c9e9b6128cce
René Calles <me@renecalles.de>
2017-02-06 21:31:41 +0100

    install package in user scheme to not require sudo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-python setup.py install --force
+python setup.py install --force --user
 mkdir -p ~/.local/share/profanity/plugins
 cp deploy/prof_omemo_plugin.py ~/.local/share/profanity/plugins/
