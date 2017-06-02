6aa6f4ab2f5e07ab7f182f1becb6758aa005fe35
René Calles <me@renecalles.de>
2017-06-02 21:23:22 +0200

    fixing install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-python setup.py install --force --user
+python setup.py install --force --user --prefix=
 mkdir -p ~/.local/share/profanity/plugins
 cp deploy/prof_omemo_plugin.py ~/.local/share/profanity/plugins/
